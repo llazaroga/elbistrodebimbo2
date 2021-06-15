16b5b370a026d38eb8fe16e7b0c54a1a0f22787c
Lucía Lázaro <lucia@ip-172-28-0-142.eu-west-1.compute.internal>
2021-06-15 23:57:26 +0200

    Change jquery and images size

--- src/index2.js
+++
@@ -1,5 +0,0 @@
-$(".page-header ul a").on("click", function (e) {
-    e.preventDefault();
-    const href = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
-  });
